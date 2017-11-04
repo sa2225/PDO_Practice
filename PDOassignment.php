84a1536202f114bc514f4c4e25dbe4e3f178e6e5
saniya anand <sa2225@afsconnect1.njit.edu>
2017-11-04 15:07:12 -0400

    testing

--- PDOassignment.php
+++ PDOassignment.php
@@ -11,5 +11,27 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "<b>Connected successfully</b>";
-echo "<br><hr>"
+echo "<br><hr>";
+
+
+
+/*$sql = "SELECT id, email, fname, lname, phone, birthday, gender, password FROM `accounts` WHERE id<6";
+*/
+
+$sql = "SELECT * from accounts";
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+   while($row = $result->fetch_assoc()) {
+$row = $result->fetch_assoc();
+        //echo "<br> id: ". $row["id"]. " -Email Address: ". $row["email"]. " - Name: ". $row["fname"]. " " . $row["lname"] . " -Phone: ". $row["phone"]. " -Birthday: ". $row["birthday"]. " -Gender: ". $row["gender"]. " -Password: ". $row["password"] . " <br>";
+    }
+} else {
+    echo "0 results";
+}
+echo $result;
+
+$conn->close();
+
 ?>
