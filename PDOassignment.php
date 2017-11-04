074db99e10a6d7b009d1df2b42aea578b0126808
saniya anand <sa2225@afsconnect1.njit.edu>
2017-11-04 13:46:34 -0400

    Added file

---
+++ PDOassignment.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "sql2.njit.edu";
+$username = "sa2225";
+$password = "7kd94gor";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
